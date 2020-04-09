4e4cf2b18f80cb52f4ecc7ba30bc1fd93921ec0e
许容瑄 <1343322562@qq.com>
2020-04-09 17:31:08 +0800

    ok

--- vue_proj_02/babel.config.js
+++ vue_proj_02/babel.config.js
@@ -1,3 +1,9 @@
+// 项目发布阶段需要用的 babel 插件
+const prodPligins = []
+if (process.env.NODE_ENV === 'production') {
+  prodPligins.push('transform-remove-console')
+}
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
@@ -9,6 +15,8 @@ module.exports = {
         libraryName: 'element-ui',
         styleLibraryName: 'theme-chalk'
       }
-    ]
+    ],
+    // 发布产品时的插件数组，去除了 console 函数
+    ...prodPligins
   ]
 }
